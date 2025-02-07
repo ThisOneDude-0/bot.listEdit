53c358bcdf4159d92297ae4a54cb6b076b21a87f
This1Dude0 <this1dude.0@gmail.com>
2025-02-07 12:44:34 -0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,4 +1,4 @@
 console.log("connected");
-document.querySelectorAll('.todoItem').forEach(item => item.addEventListener('click', (event) => {
+document.querySelectorAll('.todoItem').forEach(item => console.log(item+".eventadded"; item.addEventListener('click', (event) => {
   item.remove();
 });
